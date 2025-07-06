5a4157e08e4c5cfcf74ccaa1d0ac0f6f5dfee8b1
MFajru <fajru234@gmail.com>
2025-07-06 17:45:57 +0700

    feat: able to move left and right

--- index.js
+++ index.js
@@ -1,16 +1,69 @@
 var index = function () {
     var canvas = document.getElementById("myCanvas");
-    if (canvas == null) {
-        return;
-    }
     var ctx = canvas.getContext("2d");
-    if (ctx == null) {
-        return;
-    }
-    ctx.beginPath();
-    for (var i = 0; i < 4; i++) {
-        ctx.rect(i * 64, 448, 64, 64);
-    }
-    ctx.stroke();
+    var fallSpeed = 2;
+    var moveSpeed = 2;
+    var rectSize = 32;
+    var x = canvas.width / 2 - rectSize / 2;
+    var y = 0;
+    var direction = { left: false, right: false };
+    ctx.fillStyle = "green";
+    var rectStack = [];
+    // let fallingRect: { x: number; y: number } | null = null;
+    var rectFallAnimate = function () {
+        if (rectStack[rectStack.length - 1] &&
+            rectStack[rectStack.length - 1].y == fallSpeed) {
+            // fallingRect = null;
+            return;
+        }
+        moveBlock();
+        ctx.clearRect(0, 0, canvas.width, rectSize + y);
+        ctx.fillRect(x, y, rectSize, rectSize);
+        // fallingRect = { x: x, y: y };
+        y += fallSpeed;
+        if ((rectStack[rectStack.length - 1] &&
+            y >= rectStack[rectStack.length - 1].y - rectSize) ||
+            y > canvas.height - rectSize) {
+            rectStack.push({ x: x, y: y });
+            y = 0;
+            // fallingRect = null;
+            x = canvas.width / 2 - rectSize / 2;
+            requestAnimationFrame(rectFallAnimate);
+            return;
+        }
+        requestAnimationFrame(rectFallAnimate);
+    };
+    var moveBlock = function () {
+        if (direction.left && x > 0) {
+            x -= moveSpeed;
+            return;
+        }
+        if (direction.right && x < canvas.width - rectSize) {
+            x += moveSpeed;
+            return;
+        }
+    };
+    document.onkeydown = function (e) {
+        if (e.key == "ArrowLeft") {
+            console.log("left", x);
+            direction.left = true;
+        }
+        if (e.key == "ArrowRight") {
+            console.log("right");
+            direction.right = true;
+        }
+    };
+    document.onkeyup = function (e) {
+        if (e.key == "ArrowLeft") {
+            direction.left = false;
+        }
+        if (e.key == "ArrowRight") {
+            direction.right = false;
+        }
+    };
+    requestAnimationFrame(rectFallAnimate);
+    // if (!fallingRect) {
+    //   requestAnimationFrame(rectFallAnimate);
+    // }
 };
 index();
--- index.ts
+++ index.ts
@@ -1,22 +1,83 @@
 const index = () => {
   const canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
+  const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-  if (canvas == null) {
-    return;
-  }
+  const fallSpeed = 2;
+  const moveSpeed = 2;
+  const rectSize = 32;
+  let x = canvas.width / 2 - rectSize / 2;
+  let y = 0;
+  let direction = { left: false, right: false };
 
-  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "green";
 
-  if (ctx == null) {
-    return;
-  }
+  let rectStack: { x: number; y: number }[] = [];
+  // let fallingRect: { x: number; y: number } | null = null;
 
-  ctx.beginPath();
-  for (let i = 0; i < 4; i++) {
-    ctx.rect(i * 64, 448, 64, 64);
-  }
+  const rectFallAnimate = () => {
+    if (
+      rectStack[rectStack.length - 1] &&
+      rectStack[rectStack.length - 1].y == fallSpeed
+    ) {
+      // fallingRect = null;
+      return;
+    }
+    moveBlock();
+    ctx.clearRect(0, 0, canvas.width, rectSize + y);
+    ctx.fillRect(x, y, rectSize, rectSize);
+    // fallingRect = { x: x, y: y };
+    y += fallSpeed;
 
-  ctx.stroke();
+    if (
+      (rectStack[rectStack.length - 1] &&
+        y >= rectStack[rectStack.length - 1].y - rectSize) ||
+      y > canvas.height - rectSize
+    ) {
+      rectStack.push({ x: x, y: y });
+
+      y = 0;
+      // fallingRect = null;
+      x = canvas.width / 2 - rectSize / 2;
+      requestAnimationFrame(rectFallAnimate);
+      return;
+    }
+    requestAnimationFrame(rectFallAnimate);
+  };
+
+  const moveBlock = () => {
+    if (direction.left && x > 0) {
+      x -= moveSpeed;
+      return;
+    }
+    if (direction.right && x < canvas.width - rectSize) {
+      x += moveSpeed;
+      return;
+    }
+  };
+
+  document.onkeydown = (e) => {
+    if (e.key == "ArrowLeft") {
+      console.log("left", x);
+      direction.left = true;
+    }
+    if (e.key == "ArrowRight") {
+      console.log("right");
+      direction.right = true;
+    }
+  };
+  document.onkeyup = (e) => {
+    if (e.key == "ArrowLeft") {
+      direction.left = false;
+    }
+    if (e.key == "ArrowRight") {
+      direction.right = false;
+    }
+  };
+  requestAnimationFrame(rectFallAnimate);
+
+  // if (!fallingRect) {
+  //   requestAnimationFrame(rectFallAnimate);
+  // }
 };
 
 index();
