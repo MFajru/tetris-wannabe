59865b7b081deee86c0fa84ac74b690f434c470f
MFajru <fajru234@gmail.com>
2025-07-07 17:12:28 +0700

    try: trying to make the block not missing when is not stacked

--- index.js
+++ index.js
@@ -4,30 +4,37 @@ var index = function () {
     var fallSpeed = 2;
     var moveSpeed = 2;
     var rectSize = 32;
-    var x = canvas.width / 2 - rectSize / 2;
+    var x = canvas.width / 2;
     var y = 0;
     var direction = { left: false, right: false };
     ctx.fillStyle = "green";
     var rectStack = [];
-    // let fallingRect: { x: number; y: number } | null = null;
     var rectFallAnimate = function () {
         if (rectStack[rectStack.length - 1] &&
             rectStack[rectStack.length - 1].y == fallSpeed) {
-            // fallingRect = null;
             return;
         }
-        moveBlock();
-        ctx.clearRect(0, 0, canvas.width, rectSize + y);
+        // moveBlock();
+        // ctx.clearRect(0, 0, canvas.width, rectSize + y);
         ctx.fillRect(x, y, rectSize, rectSize);
-        // fallingRect = { x: x, y: y };
         y += fallSpeed;
-        if ((rectStack[rectStack.length - 1] &&
-            y >= rectStack[rectStack.length - 1].y - rectSize) ||
-            y > canvas.height - rectSize) {
+        // if (
+        //   (rectStack[rectStack.length - 1] &&
+        //     x != rectStack[rectStack.length - 1].x - rectSize &&
+        //     y >= rectStack[rectStack.length - 1].y - rectSize) ||
+        //   y > canvas.height - rectSize
+        // ) {
+        //   rectStack.push({ x: x, y: y });
+        //   y = 0;
+        //   // fallingRect = null;
+        //   x = canvas.width / 2 - rectSize / 2;
+        //   requestAnimationFrame(rectFallAnimate);
+        //   return;
+        // }
+        if (y > canvas.height - rectSize) {
             rectStack.push({ x: x, y: y });
             y = 0;
-            // fallingRect = null;
-            x = canvas.width / 2 - rectSize / 2;
+            x = canvas.width / 2;
             requestAnimationFrame(rectFallAnimate);
             return;
         }
@@ -35,35 +42,30 @@ var index = function () {
     };
     var moveBlock = function () {
         if (direction.left && x > 0) {
-            x -= moveSpeed;
+            x -= rectSize;
             return;
         }
         if (direction.right && x < canvas.width - rectSize) {
-            x += moveSpeed;
+            x += rectSize;
             return;
         }
     };
     document.onkeydown = function (e) {
-        if (e.key == "ArrowLeft") {
-            console.log("left", x);
-            direction.left = true;
+        if (e.key == "ArrowLeft" && x > 0) {
+            x -= rectSize;
         }
-        if (e.key == "ArrowRight") {
-            console.log("right");
-            direction.right = true;
+        if (e.key == "ArrowRight" && x < canvas.width - rectSize) {
+            x += rectSize;
         }
     };
-    document.onkeyup = function (e) {
-        if (e.key == "ArrowLeft") {
-            direction.left = false;
-        }
-        if (e.key == "ArrowRight") {
-            direction.right = false;
-        }
-    };
+    // document.onkeyup = (e) => {
+    //   if (e.key == "ArrowLeft") {
+    //     direction.left = false;
+    //   }
+    //   if (e.key == "ArrowRight") {
+    //     direction.right = false;
+    //   }
+    // };
     requestAnimationFrame(rectFallAnimate);
-    // if (!fallingRect) {
-    //   requestAnimationFrame(rectFallAnimate);
-    // }
 };
 index();
