9de1070cb82e6a19d72b5821e3f6675ca64f940b
MFajru <fajru234@gmail.com>
2025-08-07 10:18:46 +0700

    change tsconfig

--- dist/utils.js
+++
@@ -1,7 +0,0 @@
-export const getRandomElement = (list) => {
-    if (list.length <= 0) {
-        return null;
-    }
-    const randomIdx = Math.floor(Math.random() * list.length);
-    return list[randomIdx];
-};
