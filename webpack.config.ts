deb130b4b133ca944808bc02c2ec116f9d5917a8
MFajru <fajru234@gmail.com>
2025-09-07 19:25:47 +0700

    feat: update webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,64 +1,84 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin"); // automate script injection
-const webpack = require("webpack");
-const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+import { dirname, resolve } from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack";
+import FaviconsWebpackPlugin from "favicons-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import type { Configuration } from "webpack";
+import type { Argv } from "webpack-cli";
+import { fileURLToPath } from "url";
 
-module.exports = {
-  entry: "./src/index.ts", // Entry point of your TypeScript application
-  output: {
-    filename: "bundle.js", // Name of the bundled output file
-    path: path.resolve(__dirname, "dist"), // Output directory
-    clean: true, // Clean the output directory before each build
-  },
-  plugins: [
-    new FaviconsWebpackPlugin("./src/assets/favicon-32x32.png"),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html", // Path to your source index.html
-      filename: "index.html", // The output file name
-      chunks: ["main"], // Specifies which JS bundle to include
-    }),
-    // for new page
-    // new HtmlWebpackPlugin({
-    //   template: "./src/test.html", // Path to your source index.html
-    //   filename: "test.html", // The output file name
-    //   chunks: ["main"], // Specifies which JS bundle to include
-    // }),
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-    }),
-  ],
-  resolve: {
-    extensions: [".ts", ".js"], // Resolve these file extensions when importing
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/, // Apply this rule to .ts and .tsx files
-        use: "ts-loader", // Use ts-loader to transpile TypeScript
-        exclude: /node_modules/, // Exclude dependencies
-      },
-      {
-        test: /\.css$/i,
-        include: path.resolve(__dirname, "src"),
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i, // Match common image file types
-        type: "asset/resource", // Tells Webpack to emit the file and return the URL
-      },
+// recreate __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const config = (_: Record<string, any>, argv: Argv): Configuration => {
+  const isProduction = argv.mode === "production";
+  return {
+    entry: "./src/index.ts", // Entry point of your TypeScript application
+    output: {
+      filename: "bundle.js", // Name of the bundled output file
+      path: resolve(__dirname, "dist"), // Output directory
+      clean: true, // Clean the output directory before each build
+    },
+    plugins: [
+      new FaviconsWebpackPlugin("./src/assets/favicon-32x32.png"),
+      new HtmlWebpackPlugin({
+        template: "./src/index.html", // Path to your source index.html
+        filename: "index.html", // The output file name
+        chunks: ["main"], // Specifies which JS bundle to include
+      }),
+      // for new page
+      // new HtmlWebpackPlugin({
+      //   template: "./src/test.html", // Path to your source index.html
+      //   filename: "test.html", // The output file name
+      //   chunks: ["main"], // Specifies which JS bundle to include
+      // }),
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+      }),
+      isProduction &&
+        new MiniCssExtractPlugin({
+          filename: "[name].[contenthash].css",
+        }),
     ],
-  },
-  mode: "production", // Set Webpack mode (development or production)
-  devServer: {
-    static: path.resolve(__dirname, "dist"), // Serve static files from the 'dist' directory
-    port: 3000, // Serve the app on http://localhost:3000
-    open: true, // Automatically open the browser when the server starts
-    historyApiFallback: {
-      rewrites: [
-        // This one rule handles /test, /about, /contact, etc. automatically
-        { from: /^\/test$/, to: "/test.html" },
+    resolve: {
+      extensions: [".ts", ".js"], // Resolve these file extensions when importing
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/, // Apply this rule to .ts and .tsx files
+          use: "ts-loader", // Use ts-loader to transpile TypeScript
+          exclude: /node_modules/, // Exclude dependencies
+        },
+        {
+          test: /\.css$/i,
+          include: resolve(__dirname, "src"),
+          // use "style-loader" in dev to able to hot reload the css
+          // but hot reload css is not working when I'm using tailwind
+          // so, I decide just to use MinicssExtractPlugin in dev and production
+          use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i, // Match common image file types
+          type: "asset/resource", // Tells Webpack to emit the file and return the URL
+        },
       ],
     },
-  },
+    mode: isProduction ? "production" : "development", // Set Webpack mode (development or production)
+    devServer: {
+      static: resolve(__dirname, "dist"), // Serve static files from the 'dist' directory
+      port: 3000, // Serve the app on http://localhost:3000
+      open: true, // Automatically open the browser when the server starts
+      historyApiFallback: {
+        rewrites: [
+          // This one rule handles /test, /about, /contact, etc. automatically
+          { from: /^\/test$/, to: "/test.html" },
+        ],
+      },
+    },
+  };
 };
+
+export default config;
